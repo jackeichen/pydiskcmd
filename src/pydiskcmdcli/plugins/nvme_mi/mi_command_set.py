dbbd52ad9873378e526210ba9e4c1b3b866d9c24
jackeichen <eric-1128@outlook.com>
2025-08-27 10:28:58 +0800

    improve nvme mi for pydiskcmdcli

--- src/pydiskcmdcli/plugins/nvme_mi/mi_command_set.py
+++ src/pydiskcmdcli/plugins/nvme_mi/mi_command_set.py
@@ -3,14 +3,69 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 import os,sys
 import optparse
+from collections.abc import Iterable
 from pydiskcmdlib.utils import init_device
 from pydiskcmdlib.pynvme.nvme import NVMe
 from pydiskcmdcli.scripts import parser_update,script_check
 from pydiskcmdcli.utils.format_print import format_dump_bytes,json_print,format_print_pyobj
 from pydiskcmdcli.nvme_spec import decode_commands_supported_and_effects,decode_mi_commands_supported_and_effects,get_nvme_mi_command_name
-from .mi_decode import decode_subsys_health_status
+from .mi_decode import decode_subsys_health_status,decode_ctrl_health_status
 from pydiskcmdlib.exceptions import CommandReturnDataError,CommandReturnStatusError
+from pydiskcmdlib.pynvme.nvme_command import build_int_by_bitmap
 
+
+ResponseMessageStatus = {0x00: "Success",                  # Success Response
+                         0x01: "More Processing Required", # Success Response
+                         0x02: "Internal Error",
+                         0x03: "Invalid Command Opcode",
+                         0x04: "Invalid Parameter",
+                         0x05: "Invalid Command Size",
+                         0x06: "Invalid Command Input Data Size",
+                         0x07: "Access Denied",
+                         0x08: "Unable to Abort",
+                         tuple(range(0x09, 0x20)): "Reserved_0",
+                         0x20: "VPD Updates Exceeded",
+                         0x21: "PCIe Inaccessible",
+                         0x22: "Management Endpoint Buffer Cleared Due to Sanitize",
+                         0x23: "Enclosure Services Failure",
+                         0x24: "Enclosure Services Transfer Failure",
+                         0x25: "Enclosure Failure",
+                         0x26: "Enclosure Services Transfer Refused",
+                         0x27: "Unsupported Enclosure Function",
+                         0x28: "Enclosure Services Unavailable",
+                         0x29: "Enclosure Degraded",
+                         0x2A: "Sanitize In Progress",
+                         tuple(range(0x2B, 0xE0)): "Reserved_1",
+                         tuple(range(0xE0, 0x100)): "Vendor Specific",
+                         }
+
+def get_response_message(return_code):
+    if return_code in ResponseMessageStatus:
+        return ResponseMessageStatus[return_code]
+    else:
+        for i in ResponseMessageStatus.keys():
+            if isinstance(i, Iterable) and return_code in i:
+                return ResponseMessageStatus[i]
+    return "Unknown Response Message"
+
+def check_mi_response(dw0: int):
+    status = dw0 & 0xFF
+    result = {"status": status, 
+              "additional_info": (dw0>>8)&0xFFFFFF,
+              "message": get_response_message(status),  # Generic Error Response message
+              }
+    if status == 0x04:  # Invalid Parameter Error Response
+        result["message"] = "%s(Byte Location: %#x, Bit Location: %#x)" % (result["message"], 
+                                                                           result["additional_info"] & 0x07,
+                                                                           result["additional_info"] >> 8,
+                                                                           )
+    elif status == 0x01:  # More Processing Required Response
+        result["message"] = "%s(worst case time: %d ms)" % (result["message"], 
+                                                            (result["additional_info"] >> 8)/10,
+                                                            )
+    if status > 1:
+        raise CommandReturnStatusError("Tunneled Status code is %#x(%s)" % (status, result["message"]))
+    return result
 
 def _nvme_mi_print_help():
     if len(sys.argv) > 3 and sys.argv[3] in plugin_nvme_mi_commands_dict:
@@ -28,6 +83,7 @@ def _nvme_mi_print_help():
         print ("  check-support                 Check device if support NVMe MI commands")
         print ("  mi-data                       Read NVMe-MI Data Structure")
         print ("  subsys-health                 Get Subsystem Health Status")
+        print ("  ctrl-health                   Get Controller Health Status")
         print ("  vpd-read                      Read VPD page")
         print ("  ib-send                       In-Band NVMe-MI Send Command passthrough")
         print ("  ib-recv                       In-Band NVMe-MI Receive Command passthrough")
@@ -116,9 +172,11 @@ def _ib_send():
         with NVMe(init_device(dev, open_t='nvme')) as d:
             cmd = d.nvme_mi_send(options.opcode, options.nmd0, options.nmd1, data=raw_data)
         cmd.check_return_status(raise_if_fail=True)
+        mi_response = check_mi_response(cmd.cq_cmd_spec)
         #
-        print ("Tunneled Status: %#x" % (cmd.cq_cmd_spec & 0xFF))
-        print ("Tunneled NVMe Management Response: %#x" % (cmd.cq_cmd_spec >> 8))
+        print ("Tunneled Status: %#x" % mi_response["status"])
+        print ("Tunneled NVMe Management Response: %#x" % mi_response["additional_info"])
+
     else:
         parser.print_help()
 
@@ -145,9 +203,10 @@ def _ib_recv():
         with NVMe(init_device(dev, open_t='nvme')) as d:
             cmd = d.nvme_mi_recv(options.opcode, options.nmd0, options.nmd1, data_len=options.data_len)
         cmd.check_return_status(raise_if_fail=True)
+        mi_response = check_mi_response(cmd.cq_cmd_spec)
         #
-        print ("Tunneled Status: %#x" % (cmd.cq_cmd_spec & 0xFF))
-        print ("Tunneled NVMe Management Response: %#x" % (cmd.cq_cmd_spec >> 8))
+        print ("Tunneled Status: %#x" % mi_response["status"])
+        print ("Tunneled NVMe Management Response: %#x" % mi_response["additional_info"])
         if options.data_len > 0:
             print ("Response Data:")
             print ("")
@@ -189,11 +248,9 @@ def _read_nvme_mi_data():
                 cmd = d.nvme_mi_recv(0x00, numd0, nmd1, data_len=options.data_len)
             cmd.check_return_status(raise_if_fail=True)
             # check the Tunneled Status
-            if cmd.cq_cmd_spec & 0xFF:
-                raise CommandReturnStatusError("Tunneled Status code is %#x" % (cmd.cq_cmd_spec & 0xFF))
-            # check the request data
-            if options.data_len > 0 and ((cmd.cq_cmd_spec >> 8) & 0xFFFF == 0x00):
-                raise CommandReturnDataError("Return Data Length is 0")
+            mi_response = check_mi_response(cmd.cq_cmd_spec)
+            if options.data_len > 0 and (mi_response["additional_info"] & 0xFFFF) == 0:
+                raise CommandReturnDataError("Return Data Length is 0(except %d)" % options.data_len)
         else:
             raise NotImplementedError("Out-Of-Band Message Servicing Mode(%s) is not implemented now" % options.servicing_mode)
         # check the request data
@@ -220,14 +277,13 @@ def _nvm_subsys_health_status():
         ## check device
         dev = sys.argv[3].strip()
         ##
-        if options.clear_status:
-            nmd1 = 0x8000
-        else:
-            nmd1 = 0
+        nmd1 = 0x80000000 if options.clear_status else 0
         if options.servicing_mode == 'ib':
             with NVMe(init_device(dev, open_t='nvme')) as d:
                 cmd = d.nvme_mi_recv(0x01, 0, nmd1, data_len=8)
             cmd.check_return_status(raise_if_fail=True)
+            # check the Tunneled Status
+            mi_response = check_mi_response(cmd.cq_cmd_spec)
         else:
             raise NotImplementedError("Out-Of-Band Message Servicing Mode(%s) is not implemented now" % options.servicing_mode)
         ##
@@ -236,11 +292,73 @@ def _nvm_subsys_health_status():
             format_print_pyobj(result)
         elif options.output_format == "json":
             result = decode_subsys_health_status(cmd.data)
-            json_print(result)
+            json_print({"mi response": mi_response, "message data": result})
         elif options.output_format == "hex":
             format_dump_bytes(cmd.data, byteorder="obverse")
         else:
             print(cmd.data)
+    else:
+        parser.print_help()
+
+def _nvm_ctrl_health_status():
+    usage="usage: %prog mi ctrl-health <device> [OPTIONS]"
+    parser = optparse.OptionParser(usage)
+    parser.add_option("-s", "--sctlid", type="int", dest="sctlid", default=0,
+        help="Starting Controller ID. Default 0")
+    parser.add_option("-n", "--maxrent", type="int", dest="maxrent", default=0,
+        help="The maximum number of Controller Health data structure entries(0's based). Default 0")
+    parser.add_option("-f", "--function", type="int", dest="function", default=0x01,
+        help="Included function bitmap, bit map order(0-n): non-SR-IOV PCI, SR-IOV Physical, SR-IOV Virtual. Default 1")
+    parser.add_option("-a", "--all", dest="report_all", action="store_true", default=False,
+        help="Ignore the error selection bits flag. Default False")
+    parser.add_option("-k", "--key", type="int", dest="key", default=0x1F,
+        help="Key indicator bitmap returned by Controller Health data structure, bit map order: Controller Status,Composite Temperature,Percentage Used,Available Spare,Critical Warning. Default 0x1F")
+    parser.add_option("-c", "--ccf", dest="ccf", action="store_true",default=False,
+        help="Clear Changed Flags. Default False")
+    parser.add_option("-m", "--servicing-mode", type="choice", dest="servicing_mode", action="store", choices=["ib", "vdm", "smbus", "i2c"], default="ib",
+        help="Message Servicing Mode, In-Band(ib) or Out-Of-Band(vdm/smbus/i2c). Default ib")
+    parser_update(parser, add_output=["normal", "hex", "raw", "json"])
+
+    if len(sys.argv) > 3:
+        (options, args) = parser.parse_args(sys.argv[3:])
+        ##
+        script_check(options, admin_check=True)
+        ## check device
+        dev = sys.argv[3].strip()
+        ##
+        nmd0 = build_int_by_bitmap({
+            "SCTLID": (0xFFFF, 0, options.sctlid),
+            "MAXRENT": (0xFF, 2, options.maxrent),
+            "FUNCTION": (0x07, 3, options.function),
+            "ALL": (0x80, 3, 1 if options.report_all else 0),
+        })
+        nmd1 = build_int_by_bitmap({
+            "KEY": (0x1F, 0, options.key),
+            "CCF": (0x80, 3, 1 if options.ccf else 0),
+        })
+
+        if options.servicing_mode == 'ib':
+            with NVMe(init_device(dev, open_t='nvme')) as d:
+                cmd = d.nvme_mi_recv(0x02, nmd0, nmd1, data_len=options.maxrent*16+16)
+            cmd.check_return_status(raise_if_fail=True)
+            # check the Tunneled Status
+            mi_response = check_mi_response(cmd.cq_cmd_spec)
+            if (mi_response["additional_info"] >> 16) == 0:
+                raise CommandReturnDataError("Data entry number is 0")
+            data = cmd.data[:(mi_response["additional_info"] >> 16)*16]
+        else:
+            raise NotImplementedError("Out-Of-Band Message Servicing Mode(%s) is not implemented now" % options.servicing_mode)
+        ##
+        if options.output_format == "normal":
+            result = decode_ctrl_health_status(data)
+            format_print_pyobj(result)
+        elif options.output_format == "json":
+            result = decode_ctrl_health_status(data)
+            json_print({"mi response": mi_response, "message data": result})
+        elif options.output_format == "hex":
+            format_dump_bytes(data, byteorder="obverse")
+        else:
+            print(data)
     else:
         parser.print_help()
 
@@ -268,6 +386,8 @@ def _vpd_read():
             with NVMe(init_device(dev, open_t='nvme')) as d:
                 cmd = d.nvme_mi_recv(0x05, nmd0, nmd1, data_len=options.data_len)
             cmd.check_return_status(raise_if_fail=True)
+            # check the Tunneled Status
+            mi_response = check_mi_response(cmd.cq_cmd_spec)
         else:
             raise NotImplementedError("Out-Of-Band Message Servicing Mode(%s) is not implemented now" % options.servicing_mode)
         ##
@@ -283,6 +403,7 @@ plugin_nvme_mi_commands_dict = {
     "check-support": _check_support,
     "mi-data": _read_nvme_mi_data,
     "subsys-health": _nvm_subsys_health_status,
+    "ctrl-health": _nvm_ctrl_health_status,
     "vpd-read": _vpd_read,
     "ib-send": _ib_send,
     "ib-recv": _ib_recv,
--- src/pydiskcmdcli/plugins/nvme_mi/mi_decode.py
+++ src/pydiskcmdcli/plugins/nvme_mi/mi_decode.py
@@ -23,3 +23,25 @@ def decode_subsys_health_status(data):
     result["Percentage Drive Life Used"] = data[3]
     result["Composite Controller Status"] = data[4] + (data[5] << 8)
     return result
+
+def decode_ctrl_health_status(data):
+    """
+    Decode the Controller Health Status data.
+
+    :param data: The raw data bytes.
+    :return: A dictionary containing the decoded values.
+    """
+    result = []
+    for i in range(0, len(data), 16):
+        temp_data = data[i:i+16]
+        temp_result = {}
+        temp_result["Controller Identifier"] = temp_data[0] + (temp_data[1] << 8)
+        temp_result["Controller Status"] = temp_data[2] + (temp_data[3] << 8)
+        temp_result["Composite Temperature"] = temp_data[4] + (temp_data[5] << 8)
+        temp_result["Percentage Used"] = temp_data[6]
+        temp_result["Available Spare"] = temp_data[7]
+        temp_result["Critical Warning"] = temp_data[8]
+        temp_result["Controller Health Status Changed"] = temp_data[9] + (temp_data[10] << 8)
+        result.append(temp_result)
+    return result
+
